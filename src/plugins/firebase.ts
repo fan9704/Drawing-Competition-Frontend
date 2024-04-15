77957c0ef7dead1b5f060bfad9765a9b8842c05d
fan9704 <cxz123499@gmail.com>
2024-04-15 08:11:53 +0800

    ✨ feat(Plugins): Register Plugins
    Firebase/Axios/Pinia Plugins Registed in Main.ts

---
+++ src/plugins/firebase.ts
@@ -0,0 +1,10 @@
+import { initializeApp } from 'firebase/app'
+export const firebaseApp = initializeApp({
+  // your application settings
+  apiKey: "AIzaSyAhoeTpayLSyYS09o0opXu1xOsQ1FzX3cM",
+  authDomain: "equipment-system-a1ebb.firebaseapp.com",
+  projectId: "equipment-system-a1ebb",
+  storageBucket: "equipment-system-a1ebb.appspot.com",
+  messagingSenderId: "217822929499",
+  appId: "1:217822929499:web:aaa911011dd70fe8ba557e"
+})
